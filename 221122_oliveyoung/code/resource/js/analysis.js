ea27a25adafaa96dbb798c912eb073731fb937cb
yspark <b141020@gmail.com>
2022-11-23 09:50:38 +0900

    add 1123 0950

--- 221122_oliveyoung/code/resource/js/analysis.js
+++ 221122_oliveyoung/code/resource/js/analysis.js
@@ -19,9 +19,41 @@ $(document).ready(function(){
     windowHtSize();
 
     //클릭시 열기
+
+    //지역 | 매장 선택
     $( '.select_main' ).click( function() {
-    $(this).toggleClass('on');
-    $('.select_main_show').toggleClass('on');
+        $(this).toggleClass('on');
+        $('.select_main_show').toggleClass('on');
       } );
+    
+      //유저 navigation 버튼 (오른쪽 최상단 클릭)
+    $( '#showUserNavi' ).click( function() {
+        $(this).toggleClass('on');
+        $('.user_navi_ul').toggleClass('on');
+      } );
+      
+      //지역분석 > 집객시설확인 작은 버튼
+    $( '.btn_allin > .btn4').click( function() {
+        $(this).toggleClass('on');
+      } );
+
+      //하트 누르고 빼기
+    $( '.heart_a').click( function() {
+        $(this).toggleClass('on');
+      } );
+
+    // 장소정보 > 업종선택 > 1차 카테고리(업종) 선택시 2차 카테고리 SHOW
+        $(".tab_box > ul > li > a").on("click", function(){
+            var num = $(".tab_box > ul > li > a").index($(this));
+            var num2 = num+1;
+            
+            // tab_box에 on 넣기
+           $(".tab_box > ul > li").removeClass("on");
+            $('.tab_box > ul > li:eq(' + num + ')').addClass("on");
+            
+            // tab_box index 맞춰서 pc_sheet 보이기
+            $('.pc_sheet').removeClass("on");
+            $('.here' + num2 + '').addClass("on");
+        });
 
 });
